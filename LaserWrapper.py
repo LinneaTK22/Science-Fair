6ce02fa28df978ee298b538482c2cc1904f91a5f
LinneaTK22 <linnea.persson.gbg22@amerikanskagymnasiet.se>
2025-05-14 21:49:28 +0200

    y_min

--- LaserWrapper.py
+++ LaserWrapper.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from Laser import Laser
 import json
 import datetime
@@ -13,7 +11,7 @@ start_time = datetime.datetime.now()
 run_time = 0
 engage = False
 
-default_configuration = '{"run_time": 100, "min_movement": 10, "x_min": 0, "x_max": 90, "y_min": 0, "y_max": 30}'
+default_configuration = '{"run_time": 100, "min_movement": 10, "x_min": 0, "x_max": 90, "y_min": 5, "y_max": 30}'
 
 def initiateLaserSequence():
     global engage
